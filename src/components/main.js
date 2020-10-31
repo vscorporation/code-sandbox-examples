9ff471a56a8e16836e40f149c3cb721dd0fa7b0a
Василий <vscorporation@mail.ru>
2020-10-31 15:19:23 +0200

    Update main.js

--- src/components/main.js
+++ src/components/main.js
@@ -2,10 +2,6 @@ import React, { Component } from 'react'
 import MainHeader from './main-header';
 import MainNews from './main-news';
 
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-
 class Main extends Component {
     render() {
     return (
